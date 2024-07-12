06f03b559b89119c0871d3d16cec7b133ac0fd70
John Swärd <johnsvard@outlook.com>
2024-07-12 16:45:01 +0200

    kom igen!

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,7 @@ app.use('/api/posts', postRoutes);
 app.use('/api/subscribers', subscriberRoutes);
 app.use('/api/auth', authRoutes); // Ensure this line is present
 
+
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
