47ec05eedb8ee0320b2a18e4cbfd09391357abbc
John Swärd <johnsvard@outlook.com>
2024-07-12 17:51:54 +0200

    awda

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-// backend/server.js
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -9,25 +8,36 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
 
+const allowedOrigins = [
+  'https://francas-frontend.vercel.app', // Add any other allowed origins here
+  'https://francas-backend.vercel.app' // If you have a custom domain
+];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+}));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const postRoutes = require('./routes/posts');
 const subscriberRoutes = require('./routes/subscribers');
-const authRoutes = require('./routes/auth'); // Ensure this line is present
+const authRoutes = require('./routes/auth');
 
 app.use('/api/posts', postRoutes);
 app.use('/api/subscribers', subscriberRoutes);
-app.use('/api/auth', authRoutes); // Ensure this line is present
+app.use('/api/auth', authRoutes);
 
-console.log("MONGO_URI:", process.env.MONGODB_URI);
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
