c1cbe6ca5ebfc85fda4e3d44e8f13e75bf7fcdd8
John Swärd <johnsvard@outlook.com>
2024-07-12 16:49:04 +0200

    changes

--- backend/server.js
+++ backend/server.js
@@ -12,20 +12,20 @@ app.use(express.json());
 
 // Update CORS configuration to allow requests from your frontend's origin
 const allowedOrigins = [
-  'https://francas-frontend-mqw0wtuoy-john-swards-projects.vercel.app', // Your frontend's origin
-  'https://francas-backend-8z0gy2umo-john-swards-projects.vercel.app' // Your backend's origin
+  'https://francas-frontend-3legar8tw-john-swards-projects.vercel.app', // Your frontend's origin
+  'https://francas-backend-cr4nr0wiz-john-swards-projects.vercel.app' // Your backend's origin
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-}));
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+  }));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
@@ -39,8 +39,8 @@ app.use('/api/auth', authRoutes); // Ensure this line is present
 
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
