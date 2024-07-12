b6cdc289278997ad15442ee3f61652a593e7a7c8
John Swärd <johnsvard@outlook.com>
2024-07-12 15:08:31 +0200

    test

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const Subscriber = require('../models/Subscriber');
-const upload = require('../middleware/Upload');
+const upload = require('../middleware/upload');
 const sendEmail = require('../utils/mailer');
 
 async function getPost(req, res, next) {
