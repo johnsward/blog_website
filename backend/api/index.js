4b5aa0da82f71aed5f294d8525843e07c8d60958
John Swärd <johnsvard@outlook.com>
2024-07-15 16:28:53 +0200

    awd

--- backend/api/index.js
+++ backend/api/index.js
@@ -9,22 +9,28 @@ dotenv.config(); // Load environment variables
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+// Root route to check if the server is running
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+// Serve static files from the 'uploads' directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-const postRoutes = require('../routes/posts');
-const subscriberRoutes = require('../routes/subscribers');
-const authRoutes = require('../routes/auth');
+// Import and use routes
+const postRoutes = require('./routes/posts');
+const subscriberRoutes = require('./routes/subscribers');
+const authRoutes = require('./routes/auth');
 
 app.use('/api/posts', postRoutes);
 app.use('/api/subscribers', subscriberRoutes);
 app.use('/api/auth', authRoutes);
 
+// Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
+// Start the server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
