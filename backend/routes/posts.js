9012b1dfccef708ab5c7e1048e1c9dc397684c4e
John Swärd <johnsvard@outlook.com>
2024-07-16 00:34:57 +0200

    hmm

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -5,20 +5,14 @@ const Subscriber = require('../models/Subscriber');
 const upload = require('../middleware/upload');
 const sendEmail = require('../utils/mailer');
 
-async function getPost(req, res, next) {
-    let post;
+router.get('/', async (req, res) => {
     try {
-        post = await Post.findById(req.params.id);
-        if (post === null) {
-            return res.status(404).json({ message: 'Cannot find post' });
-        }
+        const posts = await Post.find();
+        res.json(posts);
     } catch (err) {
-        return res.status(500).json({ message: err.message });
+        res.status(500).json({ message: err.message });
     }
-
-    res.post = post;
-    next();
-}
+});
 
 //Get a single post by ID
 router.get('/:id', async (req, res) => {
