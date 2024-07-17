96060341016c8caaba7879f636dda014ba6b369d
John Swärd <johnsvard@outlook.com>
2024-07-17 12:50:37 +0200

    just a friendly indication

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const postRoutes = require('./routes/posts');
